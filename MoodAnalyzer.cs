c9cd49a85bd9a34a609153fe6ac8da61c6ab52dc
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-04 18:09:31 +0530

    added: Mood Analyzer logic

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21Reflections
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+        public string happy = "I am happy";
+        public string sad = "I am sad";
+
+        public MoodAnalyzer()
+        {
+
+        }
+
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            if (string.IsNullOrEmpty(this.message))
+            {
+                throw new MoodAnalysisException("Exception Raised: No message was passed to determine user's mood!");
+            }
+            else if (this.message.Contains("happy"))
+            {
+                return "happy";
+            }
+            else if (this.message.Contains("sad"))
+            {
+                return "sad";
+            }
+            return "";
+        }
+    }
+}
