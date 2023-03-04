9656cb899b601d16f6d3374a0cd03ef9ddc937bf
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-04 17:52:08 +0530

    added: using reflection for moodAnalyzer

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day21Reflections
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                AnalyzeMood mood = MoodAnalyzerFactory.CreateMoodAnalyzer();
+                Console.WriteLine(mood.Analyze(""));
+            }
+            catch (MoodAnalysisException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
