73e13f3c227b393e47ce0dd7f7f0bb7f9445414e
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-04 18:27:13 +0530

    added: using reflection for moodAnalyzer

--- Program.cs
+++ Program.cs
@@ -7,9 +7,16 @@
             try
             {
                 MoodAnalyzer moodAnalyzer = MoodAnalyzerFactory.CreateMoodAnalyzerObject("I am happy");
-                Console.WriteLine("Original Mood: " + moodAnalyzer.AnalyzeMood());
-                MoodAnalyzerFactory.ModifyMessage("I am sad now", moodAnalyzer);
-                Console.WriteLine("Modified Mood: " + moodAnalyzer.AnalyzeMood());
+                if (moodAnalyzer != null)
+                {
+                    Console.WriteLine("Original Mood: " + moodAnalyzer.AnalyzeMood());
+                    MoodAnalyzerFactory.ModifyMessage("I am sad now", moodAnalyzer);
+                    Console.WriteLine("Modified Mood: " + moodAnalyzer.AnalyzeMood());
+                }
+                else
+                {
+                    Console.WriteLine("Failed to create MoodAnalyzer object");
+                }
             }
             catch (MoodAnalysisException ex)
             {
