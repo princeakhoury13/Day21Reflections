68b76859d9093d0ce7a38ef76569c7d17399c7aa
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-04 17:58:51 +0530

    added: reflections

---
+++ MoodAnalyzerFactory .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21Reflections
+{
+    public class MoodAnalyzerFactory
+    {
+        public static AnalyzeMood CreateMoodAnalyzer()
+        {
+            try
+            {
+                Type type = Type.GetType("Day21Reflections.AnalyzeMood");
+                AnalyzeMood moodAnalyzer = (AnalyzeMood)Activator.CreateInstance(type);
+                return moodAnalyzer;
+            }
+            catch (ArgumentNullException)
+            {
+                throw new MoodAnalysisException("Exception Raised: Class not found!");
+            }
+            catch (Exception)
+            {
+                throw new MoodAnalysisException("Exception Raised: Constructor is not found!");
+            }
+        }
+    }
+}
